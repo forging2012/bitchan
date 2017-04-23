63e30ae0e81ff2f551b1f786930f60ddf42a3e58
peryaudo <peryaudo@gmail.com>
2017-04-23 15:30:12 +0900

    Add blockchain struct

--- main.go
+++ main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
-	// "github.com/golang/leveldb"
+	// "github.com/syndtr/goleveldb"
 	// "google.golang.org/grpc"
-	// "github.com/peryaudo/bitchan/pb"
+	"encoding/hex"
+	pb "github.com/peryaudo/bitchan/bitchan_pb"
 	"html/template"
 	"log"
 	"net/http"
+	"regexp"
 	"time"
-	"strings"
 )
-
 
 type BoardListItem struct {
 	Name string
-	Id string
+	Id   string
 }
 
 var boards = []BoardListItem{{Name: "ビットちゃん板", Id: "bitchan"}}
-
 
 // - Write converter from blockchain format to these view structs (top and read.cgi)
 // - Write bbs.cgi to generate blockchain format
@@ -32,17 +31,17 @@ var boards = []BoardListItem{{Name: "ビットちゃん板", Id: "bitchan"}}
 // https://pdos.csail.mit.edu/~petar/papers/maymounkov-kademlia-lncs.pdf
 
 type Board struct {
-	Id		string
-	BoardName       string
-	Threads         []Thread
+	Id        string
+	BoardName string
+	Threads   []Thread
 }
 
 type Thread struct {
-	Index int
+	Index   int
 	BoardId string
-	Hash string
-	Title string
-	Posts []Post
+	Hash    string
+	Title   string
+	Posts   []Post
 }
 
 type Post struct {
@@ -53,40 +52,105 @@ type Post struct {
 	Content   string
 }
 
-func formatTimestamp(timestamp int64) string {
-	return time.Unix(timestamp, 0).Format("2006/01/02 15:04:05")
+type BlockHash [32]byte
+type BlockBodyHash [32]byte
+type TransactionHash [20]byte
+type PostHash [20]byte
+
+type Block struct {
+	pb.BlockHeader
+	pb.BlockBody
 }
 
-func httpHandler(w http.ResponseWriter, r *http.Request) {
-	funcMap := template.FuncMap{ "formatTimestamp": formatTimestamp}
-	hogeThread := Thread{
-		Index: 1,
+type Blockchain struct {
+	Blocks    map[BlockHash]Block
+	Posts     map[PostHash]pb.Post
+	LastBlock BlockHash
+}
+
+func (b *Blockchain) ListPostHashOfThread(boardId string, threadHash TransactionHash) []PostHash {
+	return []PostHash{}
+}
+
+func (b *Blockchain) ListPostHashOfBoard(boardId string) []PostHash {
+	return []PostHash{}
+}
+
+func (b *Blockchain) ConstructThread(boardId string, threadHash TransactionHash) Thread {
+	return Thread{
+		Index:   1,
 		BoardId: "bitchan",
-		Hash: "114514",
-		Title: "ほげほげスレ",
+		Hash:    hex.EncodeToString(threadHash[:]),
+		Title:   "ほげほげスレ",
+		Posts: []Post{
+			Post{Index: 1, Name: "名無しさん", Mail: "", Timestamp: 0, Content: "1got"},
+			Post{Index: 2, Name: "名無しさん", Mail: "sage", Timestamp: 0, Content: "糞スレsage"}}}
+
+}
+
+func (b *Blockchain) ConstructBoard(boardId string) Board {
+	var threadHash TransactionHash
+	hogeThread := Thread{
+		Index:   1,
+		BoardId: "bitchan",
+		Hash:    hex.EncodeToString(threadHash[:]),
+		Title:   "ほげほげスレ",
 		Posts: []Post{
 			Post{Index: 1, Name: "名無しさん", Mail: "", Timestamp: 0, Content: "1got"},
 			Post{Index: 2, Name: "名無しさん", Mail: "sage", Timestamp: 0, Content: "糞スレsage"}}}
 
 	hageThread := Thread{
-		Index: 2,
+		Index:   2,
 		BoardId: "bitchan",
-		Hash: "114515",
-		Title: "はげ",
+		Hash:    hex.EncodeToString(threadHash[:]),
+		Title:   "はげ",
 		Posts: []Post{
 			Post{Index: 1, Name: "名無しさん", Mail: "", Timestamp: 0, Content: "てますか？"}}}
 
-	hogeBoard := Board{Id: "bitchan", BoardName: "ビットちゃん板", Threads: []Thread{hogeThread, hageThread}}
+	return Board{Id: "bitchan", BoardName: "ビットちゃん板", Threads: []Thread{hogeThread, hageThread}}
+}
+
+var blockchain Blockchain
+
+func formatTimestamp(timestamp int64) string {
+	return time.Unix(timestamp, 0).Format("2006/01/02 15:04:05")
+}
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	funcMap := template.FuncMap{"formatTimestamp": formatTimestamp}
+
+	threadMatch := regexp.MustCompile("^/test/read\\.cgi/([a-zA-Z0-9]+)/([a-fA-F0-9]+)/?").FindStringSubmatch(r.URL.Path)
+	boardMatch := regexp.MustCompile("^/([a-zA-Z0-9]+)/?$").FindStringSubmatch(r.URL.Path)
 
 	if r.URL.Path == "/" {
 		tmpl := template.Must(template.New("index.html").Funcs(funcMap).ParseFiles("index.html"))
 		tmpl.Execute(w, boards)
-	} else if strings.HasPrefix(r.URL.Path, "/test/read.cgi") {
-		tmpl := template.Must(template.New("thread.html").Funcs(funcMap).ParseFiles("thread.html"))
-		tmpl.Execute(w, hogeThread)
-	} else {
+	} else if len(boardMatch) == 2 {
+		boardName := boardMatch[1]
+
 		tmpl := template.Must(template.New("board.html").Funcs(funcMap).ParseFiles("board.html"))
-		tmpl.Execute(w, hogeBoard)
+		tmpl.Execute(w, blockchain.ConstructBoard(boardName))
+	} else if len(threadMatch) == 3 {
+		boardName := threadMatch[1]
+		threadHashSlice, err := hex.DecodeString(threadMatch[2])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var threadHash TransactionHash
+		if len(threadHashSlice) != len(threadHash) {
+			http.Error(w, "invalid thread hash length", http.StatusInternalServerError)
+			return
+		}
+		copy(threadHash[:], threadHashSlice[0:len(threadHash)])
+
+		tmpl := template.Must(template.New("thread.html").Funcs(funcMap).ParseFiles("thread.html"))
+		tmpl.Execute(w, blockchain.ConstructThread(boardName, threadHash))
+	} else if r.URL.Path == "/test/bbs.cgi" {
+		http.Error(w, "Not Implemented", http.StatusInternalServerError)
+	} else {
+		http.NotFound(w, r)
 	}
 }
 
